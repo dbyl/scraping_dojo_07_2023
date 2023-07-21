3dd7844b746a49ed4b8691966713cff167c180a2
dbyl <bylicki.damian@gmail.com>
2023-07-21 15:11:09 +0200

    quicksave

--- run.py
+++ run.py
@@ -18,22 +18,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import ActionChains
+
+from requests.exceptions import ConnectionError, HTTPError, MissingSchema, ReadTimeout
 
 from environment_config import CustomEnvironment
 
 
 url_to_scrap=CustomEnvironment.get_input_url()
 json_file=CustomEnvironment.get_output_file()
-proxy=CustomEnvironment.get_proxy()
-#print(proxy)
-#print(url_to_scrap)
-#print(json_file)
 
 
 class ScrapWebpage:
 
 
-    def __init__(self, url_to_scrap: str, to_json: bool=True, with_proxy: bool=True) -> None:
+    def __init__(self, url_to_scrap: str, to_json: bool=False, with_proxy: bool=False) -> None:
         self.url_to_scrap = url_to_scrap
         self.to_json = to_json
         self.with_proxy = with_proxy
@@ -41,13 +40,9 @@ class ScrapWebpage:
     def scrap_data(self) -> str:
         try:
             driver = self.proxy_activating()
-            #driver = webdriver.Chrome()
             driver.set_window_size(1400,1000)
             driver.get(self.url_to_scrap)
-            element_selector = ".quote"
-            waiting_timeout = 15
-            element_present = EC.presence_of_element_located((By.CSS_SELECTOR, element_selector))
-            WebDriverWait(driver, waiting_timeout).until(element_present)
+            #self.wait_for_quotes(driver)
             time.sleep(1)
             page = driver.page_source
             soup = BeautifulSoup(page, "html5lib")
@@ -60,16 +55,6 @@ class ScrapWebpage:
             logging.warning(f"HTTPError occured: {e}. \nMake sure that website url is valid")
         except ReadTimeout as e:
             logging.warning(f"ReadTimeout occured: {e}. \nTry again later")
-
-    def save_to_json(self) -> json:
-
-        with open(os.getenv("OUTPUT_FILE"), 'a') as f:
-            soup = self.scrap_data()
-            quotes = GetQuotes(soup).retrieve_quotes()
-            for q in quotes:
-                # Assuming each string is a valid JSON object
-                json_object = json.dumps(q)
-                f.write(json_object + '\n')
 
     def proxy_activating(self) -> None:
         try:
@@ -86,8 +71,68 @@ class ScrapWebpage:
         except Exception as e:
             logging.warning(f"Setting proxy up failed: {e}\n Tracking: {traceback.format_exc()}")
 
-    def next_page(self):
-        pass
+    def wait_for_quotes(self, driver) -> None:
+        try:
+            element_selector = ".quote"
+            waiting_timeout = 15
+            element_present = EC.presence_of_element_located((By.CSS_SELECTOR, element_selector))
+            WebDriverWait(driver, waiting_timeout).until(element_present)
+        except Exception as e:
+            logging.warning(f"Wait for quotes failed: {e}\n Tracking: {traceback.format_exc()}")
+
+
+    def save_to_json(self) -> json:
+
+        with open(os.getenv("OUTPUT_FILE"), 'a') as f:
+            soup = self.scrap_data()
+            quotes = GetQuotes(soup).retrieve_quotes()
+            for q in quotes:
+                # Assuming each string is a valid JSON object
+                json_object = json.dumps(q)
+                f.write(json_object + '\n')
+
+    def next_page_if_exists(self) -> None:
+        driver = self.proxy_activating()
+
+        try:
+            wait = WebDriverWait(driver, 2)
+            accept = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'next')))
+            accept.click()
+        except Exception as e: 
+            logging.warning(f"Failed: {e}\n Tracking: {traceback.format_exc()}")
+        
+        """
+        try:
+            next_page_button = soup.find('li', {'class': 'next'})
+            if next_page_button:
+                flag = True
+                #driver.find_element(By.CSS_SELECTOR, '.next').click()
+                #next_page_button_element = driver.find_element(By.CLASS_NAME, 'next')
+                wait = WebDriverWait(driver, 2)
+                accept = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'next')))
+                actions = ActionChains(driver)
+                actions.move_to_element(accept).click().perform()
+                #print(next_page_button_element)
+                #next_page_button.click()
+                return flag
+            else:
+                logging.info("No more pages.")
+                flag = False
+                return flag
+        except Exception as e:
+            logging.warning(f"Looking for next page failed: {e}\n Tracking: {traceback.format_exc()}")
+    """
+
+    def go_through_all_pages(self) -> List[str]:
+        list_of_quotes = list()
+        flag = True
+
+        while flag==True:
+            soup = self.scrap_data()
+            list_of_quotes.append(GetQuotes(soup).retrieve_quotes())
+            flag = self.next_page_if_exists()
+
+        return list_of_quotes
 
 class GetQuotes:
 
@@ -106,7 +151,7 @@ class GetQuotes:
 #output = ScrapWebpage(url_to_scrap)
 #print(output.scrap_data())
 
-soup = ScrapWebpage(url_to_scrap).save_to_json()
-output = GetQuotes(soup).retrieve_quotes()
-print(output)
+output = ScrapWebpage(url_to_scrap).go_through_all_pages()
+#output = GetQuotes(soup).go_through_all_pages()
+output
 
